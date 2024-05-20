70bf8a688ca6e7feeb47458b64e33113e2738ae9
Luis Palacios <lpalacios1119@gmail.com>
2024-05-19 23:52:14 -0600

    Clase Perro

---
+++ Clases/Perro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPolimorfismo.Clases
+{
+    public class Perro : Animal
+    {
+        public Perro(string nombre) : base(nombre) { }
+
+        public override void HacerSonido()
+        {
+            Console.WriteLine($"{Nombre} dice: Guau!");
+        }
+
+        public override void Comer()
+        {
+            Console.WriteLine($"{Nombre} está comiendo croquetas.");
+        }
+    }
+}
