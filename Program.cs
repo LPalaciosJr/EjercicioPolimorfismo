db654e2b304960e923ff827c6ea1c9900b34683f
Luis Palacios <lpalacios1119@gmail.com>
2024-05-19 23:54:44 -0600

    Clase Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EjercicioPolimorfismo.Clases;
+using System;
+using System.Collections.Generic;
+
+namespace EjercicioPolimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            List<Animal> animales = new List<Animal>
+            {
+                new Perro("Rex"),
+                new Gato("Michi"),
+                new Perro("Fido")
+            };
+
+            foreach (Animal animal in animales)
+            {
+                animal.HacerSonido();
+                animal.Comer();
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
