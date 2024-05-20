e328c0191138c65ed84355c91e63eff9ad11b399
Luis Palacios <lpalacios1119@gmail.com>
2024-05-19 23:48:33 -0600

    Mi primer commit, clase animal

---
+++ Clases/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPolimorfismo.Clases
+{
+    public class Animal
+    {
+        public string Nombre { get; set; }
+
+        public Animal(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public virtual void HacerSonido()
+        {
+            Console.WriteLine("El animal hace un sonido.");
+        }
+
+        public virtual void Comer()
+        {
+            Console.WriteLine($"{Nombre} está comiendo.");
+        }
+    }
+}
