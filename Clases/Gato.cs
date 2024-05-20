01033ccd72d5c92a58dfee1ca2816af3708b6de7
Luis Palacios <lpalacios1119@gmail.com>
2024-05-19 23:53:04 -0600

    Clase Gato

---
+++ Clases/Gato.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPolimorfismo.Clases
+{
+    public class Gato : Animal
+    {
+        public Gato(string nombre) : base(nombre) { }
+
+        public override void HacerSonido()
+        {
+            Console.WriteLine($"{Nombre} dice: Miau!");
+        }
+
+        public override void Comer()
+        {
+            Console.WriteLine($"{Nombre} está comiendo pescado.");
+        }
+    }
+}
